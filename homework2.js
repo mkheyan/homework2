5fa7d8a5852c571adb63dd9c784a1c21ba02e6b1
mkheyan <petros_mkheyan@edu.aua.am>
2018-09-08 16:22:56 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,15 @@
+const a = 'Hello';
+const b = ' AUA';
+const c = 2018;
+const d = 2022;
+
+console.log(a + b);
+
+if (d % c === 4) {
+    console.log("You will successfully graduate");
+    console.log(" in " + (d - c) + ' years');
+}
+else {
+    console.log("You will graduate");
+    console.log(" in " + (d - c) + ' years');
+}
